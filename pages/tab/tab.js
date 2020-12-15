a1570a51b929bcd4c6ab3368b7fbece970b94a0d
Weikun <you@example.com>
2020-12-15 15:20:52 +0800

    提交tab切换

---
+++ pages/tab/tab.js
@@ -0,0 +1,57 @@
+// pages/tab/tab.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navTab: ['全部订单','待付款','待发货', '待收货'],        
+    currentTab: 0,
+    sendList:[],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  //切换
+  currentTab: function (e) {
+    if (this.data.currentTab == e.currentTarget.dataset.idx){
+      return;
+    }
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx
+    })
+    this.data.sendList=[];
+  }
+})
